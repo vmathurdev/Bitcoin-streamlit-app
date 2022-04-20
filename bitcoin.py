3e8e5904a698b9a8fe6106f558032c677937cd49
Vinamra Mathur <vinamra.mathur@hotmail.co.in>
2022-04-19 23:32:36 -0300

    Optimized Code

--- bitcoin.py
+++ bitcoin.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.markdown("<h1 style='text-align: center; color: #008080;'><strong><u>Bitcoin Prices Application</u></strong></h1>", unsafe_allow_html=True)
 st.image("https://www.reuters.com/resizer/NG2ZJTm2O12wRboyCvuLURRZ1E8=/1200x0/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/P7RD6UHCKZPGZAOSQQZ2ZICBJQ.jpg",width=700,)
 days =  st.slider('Enter the number of days',1,365) 
-currency = st.radio("Currency",('CAD' , 'USD', 'INR'))
+currency = st.radio("Currency",('🇨🇦 CAD' , '🇺🇸 USD', '🇮🇳 INR', '🇸🇬 SGD', '🇨🇳 CNY', '🇯🇵 JPY'))
 a = requests.get('https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency='+str(currency)+'&days='+str(days)+'&interval=daily').json()         
 df = pd.DataFrame(a['prices'])
 df[0]= pd.to_datetime(df[0], unit='ms')
